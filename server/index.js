e5a5296c573d24592d9657c2770823e444c71e6c
maxyurei <maxpacheco420@gmail.com>
2026-02-17 13:18:37 -0800

    feat: initial setup-> Express server, User model, MongoDB and basic schema and endpoints created & tested

---
+++ server/index.js
@@ -0,0 +1,51 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import User from './models/User.js';
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB!'))
+.catch((err) => console.log('MongoDB connection error:', err));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from SecureVault sever! Auto reload works!');
+});
+
+app.get('/api/test', (req, res) => {
+    res.send('This is a test API endpoint!');
+});
+
+app.delete('/api/test-users', async (req, res) => {
+    try {
+        await User.deleteMany({});
+        res.json({message: 'All user deleted'});
+    } catch (error) {
+    res.status(400).json({error: error.message});
+    }
+});
+
+app.post('/api/test-user', async (req, res) => {
+    try {
+        const testUser = new User({
+            email: 'test2@example.com', 
+            password: 'temporarypassword1234',
+            savedPasswords: []
+        });
+
+        await testUser.save();
+        res.json({message: 'User created!', user: testUser});
+    } catch (error) {
+        res.status(400).json({ error: error.message});
+    }
+    
+});
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    savedPasswords: [{
+        website: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+}, {
+    timestamps: true
+});
+
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) {
+    return;
+  }
+  
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
